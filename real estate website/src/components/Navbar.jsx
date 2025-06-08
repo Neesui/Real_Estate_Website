8d8e5ce3b847db900e8f5505e9b9102d016c86e7
Neesui <neeshachy2020@gmail.com>
2025-06-08 19:53:35 +0545

    first commit

---
+++ real estate website/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className='absolute top-0 left-0 w-full'>
+        <div>
+            <img src='https://icon2.cleanpng.com/20180529/hto/avpbqaarj.webp' alt='logo' className='w-40'/>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
