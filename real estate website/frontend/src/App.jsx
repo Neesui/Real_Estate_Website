8fbbf158b687fc3cdcd0600e52a797227ee71942
Neesui <neeshachy2020@gmail.com>
2025-06-30 22:26:22 +0545

    create header components

--- real estate website/frontend/src/App.jsx
+++ real estate website/frontend/src/App.jsx
@@ -4,9 +4,12 @@ import Home from './pages/Home'
 import SignOut from './pages/SignUp'
 import SignIn from './pages/Signin'
 import Profile from './pages/Profile'
+import Header from './components/Header'
+import About from './pages/About'
 const App = () => {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
